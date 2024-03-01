edc25dee621ac9d18615a8d879ff64b9b2647f97
ngo500 <72844093+ngo500@users.noreply.github.com>
2024-03-01 12:42:16 -0800

    Update app.py, add GET method for transactions

--- app.py
+++ app.py
@@ -1,10 +1,21 @@
-# Import libraries
-
-# Instantiate Flask functionality
+from flask import Flask, request, url_for, redirect, render_template
+app = Flask(__name__)
 
 # Sample data
+transactions = [
+    {'id': 1, 'date': '2023-06-01', 'amount': 100},
+    {'id': 2, 'date': '2023-06-02', 'amount': -200},
+    {'id': 3, 'date': '2023-06-03', 'amount': 300}
+]
 
-# Read operation
+@app.route('/', , methods=['GET'])
+def get_transactions():
+    """
+    GET method to retrieve and display all transactions
+    returns:
+    always renders the transactions.html page
+    """
+    return render_template("transactions.html", transactions = transactions)
 
 # Create operation
 
